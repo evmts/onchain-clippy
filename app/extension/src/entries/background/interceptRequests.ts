5dc9c3925e700d17ad263073783563b5d071bfb8
S29 <nots29vv@gmail.com>
2025-01-01 10:33:50 +0100

    console.log transaction payload

--- app/extension/src/entries/background/interceptRequests.ts
+++ app/extension/src/entries/background/interceptRequests.ts
@@ -1,10 +1,28 @@
+interface TransactionData {
+  chainId: string; 
+  data: string;
+  from: string; 
+  gas: string;
+  gasPrice: string;
+  to: string; 
+  value: string;
+}
+
 function startMessageListener() {
-  function intercept(_message: any) {
+  function intercept(message: TransactionData) {
     console.log("Hello, World")
+    console.log(message)
+    console.log(message.chainId)
+    console.log(message.data)
+    console.log(message.from)
+    console.log(message.gas)
+    console.log(message.gasPrice)
+    console.log(message.to)
+    console.log(message.value)
   }
   chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
     if (message.message?.method != "eth_sendTransaction") return 
-    intercept(message.message)
+    intercept(message.message.params[0])
   })
 }
 
