b71f21a32725ed00474c055b2ea971b445c84855
S29 <nots29vv@gmail.com>
2025-01-02 05:38:45 +0100

    using bun to build

--- app/extension/manifest.config.ts
+++ app/extension/manifest.config.ts
@@ -6,27 +6,27 @@ export const getManifest = ({ dev }: { dev?: boolean }) => ({
     version: pkg.version,
     manifest_version: 3,
     action: {
-        default_popup: "src/popup.html",
+        default_popup: "popup.html",
         default_icon: {
             '48': `icons/icon48.png`,
             '128': `icons/icon128.png`,
         },
     },
     background: {
-      service_worker: 'src/entries/background/interceptRequests.ts'
+      service_worker: 'entries/background/interceptRequests.js'
     },
     content_scripts: [
       {
         id: "inpage",
         matches: ['*://*/*'],
-        js: ['src/entries/content/index.ts'],
+        js: ['entries/content/index.js'],
         run_at: 'document_start',
         world: 'MAIN',
       },
       {
         id: "inpage2",
         matches: ['*://*/*'],
-        js: ['src/entries/content/listen.ts'],
+        js: ['entries/content/listen.js'],
         run_at: 'document_start'
       }
     ],
